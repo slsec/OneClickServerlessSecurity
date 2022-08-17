7416d62a735c592d8b10483f83a8359e9957c6df
Seth Rait <serait@microsoft.com>
2022-08-17 15:02:39 -0700

    update policy and add ability to remove policy

--- SSAOneClickEnableMethod2.ps1
+++ SSAOneClickEnableMethod2.ps1
@@ -6,6 +6,9 @@ $WarningPreference = 'SilentlyContinue'
 # Get the subscription_id from managed identity and set it to a context
 $selected_subscription_id = $Env:subscription_id
 
+$PolicyName = "DefenderForServerless"
+$PolicyScope = "/subscriptions/$($selected_subscription_id)"
+
 # Get the tenant_id from managed identity and set it to a context
 $tenant_id = $Env:tenant_id
 
@@ -22,29 +25,33 @@ $toggle_option = $Env:toggle_option
 # remove AZURE_FUNCTIONS_SECURITY_AGENT_ENABLED to disable the agent.
 switch ($toggle_option) {
 
-# Remove Configuration Switch to Disable
-"0" { For ($Cntr = 0 ; $Cntr -lt $($function_app_list.Count); $Cntr++) {
-Remove-AzFunctionAppSetting -Name $function_app_list[$Cntr].Name -ResourceGroupName $function_app_list[$Cntr].ResourceGroupName -AppSettingName "AZURE_FUNCTIONS_SECURITY_AGENT_ENABLED" | Out-Null
-};Write-Host "Disabled AZURE_FUNCTIONS_SECURITY_AGENT for $($function_app_list.Count) Functions with Subscription ID is: $selected_subscription_id"; break }
+    # Remove Configuration Switch to Disable
+    "0" {
+        For ($Cntr = 0 ; $Cntr -lt $($function_app_list.Count); $Cntr++) {
+            Remove-AzFunctionAppSetting -Name $function_app_list[$Cntr].Name -ResourceGroupName $function_app_list[$Cntr].ResourceGroupName -AppSettingName "AZURE_FUNCTIONS_SECURITY_AGENT_ENABLED" | Out-Null
+        };
+        Write-Host "Disabled AZURE_FUNCTIONS_SECURITY_AGENT for $($function_app_list.Count) Functions with Subscription ID is: $selected_subscription_id"; break 
+        Remove-AzPolicyAssignment -Name $PolicyName -Scope $PolicyScope
+        Remove-AzPolicyDefinition -Name $PolicyName -Force
+    }
 
-# Update Configuration to Enable
-"1" { 
-Register-AzResourceProvider -ProviderNamespace 'Microsoft.PolicyInsights'    
-New-AzPolicyDefinition -Name "DefenderForServerless" -Description "Policy to deploy resources required to enable the Defender for Serverless product" -Policy <path_to_policy.json> -Mode "indexed"
-New-AzPolicyAssignment -Name "DefenderForServerless" -Description "Policy to deploy resources required to enable the Defender for Serverless product" -Scope "Subscription" -PolicyDefinition <pol_def_id> -AssignIdentity
+    # Update Configuration to Enable
+    "1" { 
+        Register-AzResourceProvider -ProviderNamespace 'Microsoft.PolicyInsights' | Out-Null  
+        $PolicyDefinition = New-AzPolicyDefinition -Name $PolicyName -Policy Policy.json -Description $PolicyDescription | Out-Null
+        $PolicyAssignment = New-AzPolicyAssignment -Name $PolicyName -Description $PolicyDescription -Scope $PolicyScope -PolicyDefinition $PolicyDefinition -IdentityType SystemAssigned -Location westus2 | Out-Null
+        Start-AzPolicyRemediation -PolicyAssignmentId $PolicyAssignment.ResourceId -Name $PolicyName -ParallelDeploymentCount 1 -ResourceDiscoveryMode ReEvaluateCompliance | Out-Null
 
-Start-AzPolicyRemediation -PolicyAssignmentId "<pol_assingment_id>" -Name "DefenderForServerless" -ParallelDeploymentCount 1 -ResourceDiscoveryMode ReEvaluateCompliance
+        For ($Cntr = 0 ; $Cntr -lt $($function_app_list.Count); $Cntr++) {
+            Update-AzFunctionAppSetting -Name $function_app_list[$Cntr].Name -ResourceGroupName $function_app_list[$Cntr].ResourceGroupName -AppSetting @{"AZURE_FUNCTIONS_SECURITY_AGENT_ENABLED" = "1" } | Out-Null
+        };
+        Write-Host "Enabled AZURE_FUNCTIONS_SECURITY_AGENT for $($function_app_list.Count) Functions with Subscription ID is: $selected_subscription_id"; 
+        break 
+    }
 
-
-For ($Cntr = 0 ; $Cntr -lt $($function_app_list.Count); $Cntr++) {
-Update-AzFunctionAppSetting -Name $function_app_list[$Cntr].Name -ResourceGroupName $function_app_list[$Cntr].ResourceGroupName -AppSetting @{"AZURE_FUNCTIONS_SECURITY_AGENT_ENABLED" = "1"} | Out-Null
-};
-Write-Host "Enabled AZURE_FUNCTIONS_SECURITY_AGENT for $($function_app_list.Count) Functions with Subscription ID is: $selected_subscription_id"; 
-break }
-
-# Defaults to break the switch, without any changes made
-default {break}
+    # Defaults to break the switch, without any changes made
+    default { break }
 }
 
-$Env::SetEnvironmentVariable("AZURE_FUNCTIONS_SECURITY_AGENT_ENABLED",$null,"User")
+$Env::SetEnvironmentVariable("AZURE_FUNCTIONS_SECURITY_AGENT_ENABLED", $null, "User")
 $WarningPreference = $WarningPrefBackup
