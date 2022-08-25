3ec7c77e1f58f0462a3139ad7cc1013a6efa0bdd
Seth Rait <srait@brandeis.edu>
2022-08-25 12:19:34 -0700

    Policy for script (#5)
    -Add Policy to Script
    * Update policy and add ability to remove policy
    * Cleanup resources after disable
    * Add customer-facing documentation
    * Add back config value
    * Add role assignment to created identity
    
    Co-authored-by: Seth Rait <serait@microsoft.com>

--- README.md
+++ README.md
@@ -1,34 +1,53 @@
 # Enable - Disable serverless security
-*This Script allows a customer to enable or remove ServerlessSecurity Agent*
+*This script allows for enabling and disabling the Serverless Security Agent*
 
-***Please note : The function apps will Restart during enabling or disabling the agent. In case of any errors/interruptions, the script can be retried, and function apps could restart in each attempt. Make sure to enter the correct subscription id and secure key***
+***Please note : All function apps will Restart during enabling or disabling the agent. In the case of any errors/interruptions the script can be retried and Function Apps may restart in each attempt. Make sure to enter the correct subscription id and secure key***
 
 
-1) Open Cloudshell and Run the command to dowload script to cloudshell
+1) Open Cloud Shell and run the following command:
 ```
-curl -LO "https://raw.githubusercontent.com/vikenparikh/OneClickServerlessSecurity/main/SSACloudShellMethod1.ps1"
+curl -LO "https://raw.githubusercontent.com/vikenparikh/OneClickServerlessSecurity/main/SSAOnboard.ps1";curl -LO "https://raw.githubusercontent.com/vikenparikh/OneClickServerlessSecurity/main/Policy.json"
 ```
 ![image](https://user-images.githubusercontent.com/20373954/185518206-65a87986-b177-41fe-9501-8cc61f41b8d0.png)
 ![image](https://user-images.githubusercontent.com/20373954/185518266-8ba216a2-a02b-455c-a7fb-c12d43f3e88d.png)
 
-2) Run the script by using the command on cloudshell
+2) Excecute the script on Cloud Shell:
 ```
-./CloudShellMethod1.ps1
+./SSAOnboard.ps1
 ```
 ![image](https://user-images.githubusercontent.com/20373954/185518120-550d6f20-ad4a-43ee-b3c2-81050fde5c01.png)
 
-3) Copy the SubscriptionId for the subscription you want to change and insert it when prompted by the script
+3) Copy your SubscriptionId for the subscription you want to change and insert it when prompted.
 
 ![image](https://user-images.githubusercontent.com/20373954/185519051-159fb921-71bb-4d1d-a18f-b770785e5cab.png)
 
-4) Then enter 0 to Disable, 1 to Enable the ServerlessSecurity Agent
+4) Enter 0 to Disable or 1 to Enable the Serverless Security Agent.
 
 ![image](https://user-images.githubusercontent.com/20373954/185519519-f8ef84a5-c076-4f9b-8697-31ade0965b1f.png)
 ![image](https://user-images.githubusercontent.com/20373954/185519700-f73e0ffb-cb19-4259-9944-348fac19ddc5.png)
 
 5) (Only For Enabling the ServerlessSecurity Agent) Enter the provided secure key
 
-6) Wait till the deployment completes and check if the script run was successful.
+6) Wait until the deployment completes and check if the script was successful.
 
 ![image](https://user-images.githubusercontent.com/20373954/185520191-ac574c27-3d32-4ba3-89f4-9bba5b6c892d.png)
 ![image](https://user-images.githubusercontent.com/20373954/185520510-2b8768d3-6f39-4a40-9e01-cd125f88a11e.png)
+
+# Effects on Resources and Subscriptions
+
+Running either method of the onboarding script will have the following effects on your resources:
+1) Register the resource provider 'Microsoft.PolicyInsights'
+2) Add the application setting 'AZURE_FUNCTIONS_SECURITY_AGENT_ENABLED' to each Azure Function in your subscription
+3) Assign a custom policy and associated remediation task, described below.
+
+Running the disable command in this repository will remove application setting from all Azure Functions, remediation task, policy assignment and policy definition.
+
+# Policy Overview
+The Azure Policy Definition in this repository will be uploaded to your subscription and assigned to the subscription scope. Additionally, a remediation task will be created for all current resources. This policy takes several actions to onboard your Functions to the Azure Functions Security Agent.
+Firstly, it creates the resource group 'mdc-slsec-rg' to house resources related to the functioning of the agent. These resources include:
+1) A Log Analytics Workspace for each region you have a FunctionApp deployed in. This LA Workspace contains events to process from your Function, as well as debug logs
+2) A Data Collection Endpoint per region, which defines where these logs go
+3) A Data Collection Rule which defines log transformations
+4) A User Assigned Identity with permissions to write to the LA Workspace. This identity is added to each of your Function Apps
+5) A resource lock on the Identity to prevent accidental deletion.
+Please do not modify or delete any of the resources in the mdc-slsec-rg, as this will stop the security agent from working. If you wish to disable the agent or delete the resources, please run the offboarding command described above.
---
+++ SSAOnboard.ps1
@@ -0,0 +1,86 @@
+#Exit Script on error
+$ErrorActionPreference = "Stop"
+$WarningPrefBackup = $WarningPreference
+$WarningPreference = 'SilentlyContinue'
+
+# Describe to the User what that Script does
+Write-Host "This is a Script to enable or disable ServerlessSecurity Plugin for Functions within a Subscription"
+
+# Prompt Customer to enter the subscription_id and set it to a context
+$selected_subscription_id = Read-Host -Prompt "`n Enter the subscription_id you would like to enable/disable the ServerlessSecurity agent for"
+Write-Host "Selected Subscription ID is: $selected_subscription_id"
+Set-AzContext -Subscription $selected_subscription_id
+
+$PolicyName = "DefenderForServerless"
+$PolicyScope = "/subscriptions/$($selected_subscription_id)"
+
+# For each function from $function_app_list, 
+# set AZURE_FUNCTIONS_SECURITY_AGENT_ENABLED to 1 to enable the agent.
+# remove AZURE_FUNCTIONS_SECURITY_AGENT_ENABLED to disable the agent.
+$toggle_option = Read-Host -Prompt "`n Enter 0 to Disable, 1 to Enable, any other input will do nothing and exit.`n Note: enablement/disablement of the defender will result in function restart...."
+
+if ($toggle_option -eq 1) {
+    $ss_config_value = Read-Host -Prompt "`n Enter secure key provided"
+}
+
+Write-Host "Please wait while deployment is complete..."
+# Get all functions within the subscription
+$function_app_list = Get-AzFunctionApp -SubscriptionId $selected_subscription_id
+
+switch ($toggle_option) {
+
+    # Remove Configuration Switch to Disable
+    0 {
+        For ($Cntr = 0 ; $Cntr -lt $($function_app_list.Count); $Cntr++) {
+            Remove-AzFunctionAppSetting -Name $function_app_list[$Cntr].Name -ResourceGroupName $function_app_list[$Cntr].ResourceGroupName -AppSettingName "AZURE_FUNCTIONS_SECURITY_AGENT_ENABLED", "SERVERLESS_SECURITY_OFFLOAD_TO_EH", "SERVERLESS_SECURITY_CONFIG" | Out-Null
+        };
+        Write-Host "Disabled AZURE_FUNCTIONS_SECURITY_AGENT for Subscription ID is: $selected_subscription_id"
+        Remove-AzPolicyAssignment -Name $PolicyName -Scope $PolicyScope
+        Remove-AzPolicyDefinition -Name $PolicyName -Force
+
+        Write-Host "Cleaning up resources. This may take a while..."
+        Remove-AzResourceLock -LockName 'CanNotDeleteLock-mdc-slsec-identity' -ResourceGroupName 'mdc-slsec-rg' -ResourceName 'mdc-slsec-identity' -ResourceType 'Microsoft.ManagedIdentity/userAssignedIdentities' -Force
+        Remove-AzResourceGroup -Name 'mdc-slsec-rg' -Force
+        break
+    }
+
+    # Update Configuration to Enable
+    1 { 
+        Register-AzResourceProvider -ProviderNamespace 'Microsoft.PolicyInsights' | Out-Null # Needed to create policies
+        $PolicyDescription = "Policy to deploy resources required to enable the Defender for Serverless product"
+        $PolicyDefinition = New-AzPolicyDefinition -Name $PolicyName -Policy Policy.json -Description $PolicyDescription
+        # The policy assignment needs to be created early on so its identity has time to propagate 
+        $PolicyAssignment = New-AzPolicyAssignment -Name $PolicyName -Description $PolicyDescription -Scope $PolicyScope -PolicyDefinition $PolicyDefinition -Location westus2 -AssignIdentity
+        
+        $UpdateFunctionAppSetting = @{}
+        $UpdateFunctionAppSetting.Add("AZURE_FUNCTIONS_SECURITY_AGENT_ENABLED", "1")
+        $UpdateFunctionAppSetting.Add("SERVERLESS_SECURITY_OFFLOAD_TO_EH", "True")
+        $UpdateFunctionAppSetting.Add("SERVERLESS_SECURITY_CONFIG", $ss_config_value)
+
+        For ($Cntr = 0 ; $Cntr -lt $($function_app_list.Count); $Cntr++) {
+            Update-AzFunctionAppSetting -Name $function_app_list[$Cntr].Name -ResourceGroupName $function_app_list[$Cntr].ResourceGroupName -AppSetting $UpdateFunctionAppSetting | Out-Null
+        };
+
+        # https://docs.microsoft.com/en-us/azure/governance/policy/how-to/remediate-resources?tabs=azure-powershell#grant-permissions-to-the-managed-identity-through-defined-roles
+        $RoleDefinitionIds = $PolicyDefinition.Properties.policyRule.then.details.roleDefinitionIds 
+        if ($RoleDefinitionIds.Count -gt 0) {
+            $RoleDefinitionIds | ForEach-Object {
+                $RoleDefId = $_.Split("/") | Select-Object -Last 1
+                try {
+                    New-AzRoleAssignment -Scope $PolicyScope -ObjectId $PolicyAssignment.Identity.PrincipalId -RoleDefinitionId $RoleDefId
+                }
+                catch [Microsoft.Azure.Management.Authorization.Models.ErrorResponseException] {
+                    "Role Assingment $RoleDefId already exists. Continuing"
+                }
+            }
+        }
+        Start-AzPolicyRemediation -PolicyAssignmentId $PolicyAssignment.ResourceId -Name $PolicyName -ParallelDeploymentCount 1 -ResourceDiscoveryMode ReEvaluateCompliance
+        Write-Host "Enabled AZURE_FUNCTIONS_SECURITY_AGENT for Subscription ID: $selected_subscription_id"; break 
+    }
+
+    # Defaults to break the switch, without any changes made
+    default { break }
+}
+
+# Reset preference
+$WarningPreference = $WarningPrefBackup
